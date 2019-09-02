522983823aa0c88dc2c7c759d31f7b40fbf28a06
Sergio Cuellar <scuellarv@kionetworks.com>
2019-09-02 16:02:55 -0500

    Use a DynamoDB table to store the counter

--- handler.py
+++ handler.py
@@ -1,16 +1,27 @@
 import tweepy
 import json
-
-counter = 5
+import boto3
 
 def countDays(event, context):
 
     # Refer to https://apps.twitter.com/
-    consumer_key = "<INSERT_consumer_key>"
-    consumer_secret = "<INSERT_consumer_secret>"
+    consumer_key = "<INSERT_CONSUMER_KEY_TWITTER>"
+    consumer_secret = "<INSERT_CONSUMER_SECRET_TWITTER>"
 
-    access_token = "<INSERT_access_token>"
-    access_token_secret = "<INSERT_access_token_secret>"
+    access_token = "<INSERT_ACCESS_TOKEN_TWITTER>"
+    access_token_secret = "<INSERT_TOKEN_SECRET_TWITTER>"
+
+    client = boto3.resource('dynamodb')
+    table = client.Table('count_days')
+    table.update_item(
+        Key = {
+            'since': '10-08-2019'
+            },
+        UpdateExpression = 'SET num_days = num_days +:incr',
+        ExpressionAttributeValues = {
+            ':incr':1
+        }
+    )
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.secure = True
@@ -18,15 +29,19 @@ def countDays(event, context):
 
     api = tweepy.API(auth)
 
-    message = "Tag "+counter
-    counter = counter + 1
+    response = table.get_item(
+        Key={
+            "since": "10-08-2019"
+        }
+    )
 
+    print(response["Item"])
+    items = response["Item"]
+
+    counter = items["num_days"]
+
+    message = "Tag "+str(counter)
+    
     api.update_status(status=message)
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+return 1
 
